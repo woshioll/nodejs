1abd67f64079f09a2c611c854970e4d1d29b40d8
wcsyangfan <2587762306@qq.com>
2019-12-19 22:33:56 +0800

    init 项目

--- src/app.js
+++ src/app.js
@@ -1,26 +1,6 @@
 const express = require("express");
 const app = express();
 
-// app.use((req,res)=>{
-//     res.json({
-//         name:'张三'
-//     })
-// })
-
-app.get('/name/:age',(req,res)=>{
-    let {age} = req.params;
-    res.json({
-        name:'tom',
-        age:age
-    })
-})
-app.get('/name',(req,res)=>{
-    res.send('get message')
-})
-app.post('/name',(req,res)=>{
-    res.send('post message')
-})
-
 app.listen(3000,()=>{
     console.log('server启动成功')
 })
